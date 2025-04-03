ac6777eb311117757ef00177645a3e787b2e88c1
B0C4S2006 <sebastian.2006.bc@gmail.com>
2025-04-03 14:31:19 -0500

    caaaaaaaaambios extraños que hay en mi

--- Assets/Scripts/ConsumableItemDataSO.cs
+++ Assets/Scripts/ConsumableItemDataSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Consumable SO", menuName = "Consmumable SO")]
+[CreateAssetMenu(fileName = "Consumable SO", menuName = "New Consmumable SO")]
 public class ConsumableItemData : ItemdataSO
 {
     public enum ConsumableTypeEnum
